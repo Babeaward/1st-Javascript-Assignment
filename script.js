4e1392272026a539d169d2ba1be879aeaceb5ff1
Babeaward <okaformerit@gmail.com>
2023-01-12 19:21:27 +0100

    my fist java script commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let age= prompt ("how old are you?")
+let birthyear= prompt ("what year were you born")
+let presentyear= age + birthyear
+let period= "We are currently in year "
+let answer =  presentyear + period
+console.log(answer)
+
+var fName = prompt ("what is your first name?")
+var lName = prompt ("what is your last name?")
+var first= fName.slice(0,4)
+var last= lName.slice(0,1)
+var name = first + last
+var id= name.toLowerCase()
+var fullId= "Your CapacityBay userid is "
+var output= fullId + id
+alert(output)
+
